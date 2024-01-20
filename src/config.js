6d2c770bbd26d25ccd8261ac5144c3f7800c5e4c
Sugger25e <sugger25e@gmail.com>
2024-01-21 06:58:37 +0800

    yes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const clientId = 'c39541571c464661a271e06815f43ce1'
 export const clientSecret = 'a2ef4c45022546df907a907ffe109e47'
-export const redirectUri = 'https://sproutdev.netlify.app/callback'
+export const redirectUri = 'http://192.168.254.105:3000/callback'
