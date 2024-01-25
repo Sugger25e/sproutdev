296961c9f6b3c44e24d16db2c70e3c9ceecd8591
Sugger25e <sugger25e@gmail.com>
2024-01-25 10:12:44 +0800

    removed loader

--- src/components/container/Navbar.js
+++ src/components/container/Navbar.js
@@ -4,29 +4,14 @@ import "../../styles/stats.css";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
-function PopupMessage({ message, onClose }) {
-  return (
-    <div className={`popup-message ${message ? 'active' : ''}`}>
-      <div className="popup-content">
-        {message && <p>{message}</p>}
-        <button onClick={onClose}>Close</button>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-      </div>
-    </div>
-  );
-}
 
 function Navbar({ accessToken }) {
   const [userName, setUserName] = useState(null);
   const [userIcon, setUserIcon] = useState(null);
   const [imageLoaded, setImageLoaded] = useState(false);
-  const [showLoginMessage, setShowLoginMessage] = useState(false);
 
   useEffect(() => {
     const fetchUserInfo = async () => {
-      try {
         const ures = await axios.get('https://api.spotify.com/v1/me', {
           headers: {
             Authorization: `Bearer ${accessToken}`,
@@ -35,9 +20,6 @@ function Navbar({ accessToken }) {
 
         setUserName(ures.data.display_name);
         setUserIcon(ures.data.images[0].url);
-      } catch (error) {
-        setShowLoginMessage(true);
-      }
     };
 
     if (accessToken) {
@@ -47,17 +29,6 @@ function Navbar({ accessToken }) {
 
   const handleImageLoad = () => {
     setImageLoaded(true);
-  };
-
-  const handlePopupClose = () => {
-    setShowLoginMessage(false);
-  };
-
-  const handleLinkClick = (event) => {
-    if (!accessToken) {
-      event.preventDefault();
-      setShowLoginMessage(true);
-    }
   };
 
   return (
@@ -71,16 +42,9 @@ function Navbar({ accessToken }) {
         </Link>
 
         <div className="nav-right">
-          <Link to="/stats" onClick={handleLinkClick}>Statistics</Link>
-          <Link to="/about" onClick={handleLinkClick}>About</Link>
+          <Link to="/stats">Statistics</Link>
+          <Link to="/about">About</Link>
         </div>
-
-        {showLoginMessage && (
-  <PopupMessage
-    message="You must login first."
-    onClose={handlePopupClose}
-  />
-)}
 
         {accessToken && (
           <Link to="/profile" style={{ textDecoration: 'none' }}>
