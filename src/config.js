de1c1b0ac3e9c3d59de93cf2b2303b29844fbebf
Sugger25e <sugger25e@gmail.com>
2024-02-12 19:36:00 +0800

    i forgor

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const clientId = 'c39541571c464661a271e06815f43ce1'
 export const clientSecret = 'a2ef4c45022546df907a907ffe109e47'
-//export const redirectUri = 'https://sproutdev.netlify.app/callback'
+export const redirectUri = 'https://sproutdev.netlify.app/callback'
 //export const redirectUri = 'http://192.168.254.105:3000/callback'
-export const redirectUri = 'http://localhost:3000/callback'
+//export const redirectUri = 'http://localhost:3000/callback'
 //export const serverUri = 'http://localhost:8080'
 export const serverUri = 'https://spdv-bknd.cyclic.app'
 export const client_id = "81dd44566bd34114be20c35328dbbf38"
