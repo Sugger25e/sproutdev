15ce4d69a7b8eef30913e97e22a61e151c87d23f
Sugger25e <sugger25e@gmail.com>
2024-02-12 19:35:15 +0800

    new backend

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ export const clientSecret = 'a2ef4c45022546df907a907ffe109e47'
 //export const redirectUri = 'https://sproutdev.netlify.app/callback'
 //export const redirectUri = 'http://192.168.254.105:3000/callback'
 export const redirectUri = 'http://localhost:3000/callback'
-export const serverUri = 'http://localhost:8080'
-//export const serverUri = 'https://spdv-bckd.cyclic.app'
+//export const serverUri = 'http://localhost:8080'
+export const serverUri = 'https://spdv-bknd.cyclic.app'
 export const client_id = "81dd44566bd34114be20c35328dbbf38"
 export const client_secret = "8892717c784c46e9851a2c7e3529cec0"
 export const regex = /(?<=(https:\/\/)?open\.spotify\.com\/(track|playlist)\/)([a-zA-Z0-9]{15,})/g 
